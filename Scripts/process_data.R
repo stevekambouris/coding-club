f1fa616558d6dc3c58d5ab96c5df7fb164d055ab
Steve Kambouris <s.kambouris@student.unimelb.edu.au>
2018-11-22 16:51:03 +1100

    Adds Dockerfile for testing with Binder

--- Scripts/process_data.R
+++ Scripts/process_data.R
@@ -1,1 +1,7 @@
 # process_data.R
+
+# Initial download of the data from original source
+
+# Import the CSV data file into R.
+raw_data <- read.csv("Data/Raw/traitdata.csv", header=TRUE, stringsAsFactors = FALSE)
+
